f54e6598efb396fb5c750164ac47196f3c05ce1e
Marcus Barnes <mavjbarnes@gmail.com>
2015-11-10 13:50:55 -0500

    app server working as expected'

---
+++ server/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('./../app/index.ejs', {});
+});
+
+app.listen(port, () => {
+	console.log(`Client server running on port ${port}`);
+});
