98129263005e2fe75bf50ca1373e84e3acf339a1
Marcus Barnes <mavjbarnes@gmail.com>
2015-11-10 15:29:27 -0500

    added express middleware for static files

--- server/main.js
+++ server/main.js
@@ -5,6 +5,9 @@ const express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
 
+// middleware for serving static files (public files)
+app.use(express.static(`${__dirname}/../app`));
+
 app.get('/', (req, res) => {
 	res.render('./../app/index.ejs', {});
 });
