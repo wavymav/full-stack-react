2e2851637946173395742f25866d3615386b7905
Marcus Barnes <mavjbarnes@gmail.com>
2015-11-10 17:33:28 -0500

    added dummy data to main.js for the TodoList compenent to render

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -3,10 +3,21 @@
 const React = require('react');
 
 let TodoList = React.createClass({
+	renderTodoItems() {
+		return this.props.tasks.map((task) => {
+			return <li>{task.todo}</li>
+		});
+	},
+
 	render() {
 		return (
 			<div>
 				<h1>Todo's</h1>
+				<div>
+					<ul>
+						{this.renderTodoItems()}
+					</ul>
+				</div>
 			</div>
 		);
 	}
--- app/main.jsx
+++ app/main.jsx
@@ -3,6 +3,18 @@
 const React = require('react');
 const TodoList = require('./components/TodoList.jsx');
 
-React.render(<TodoList />, app);
+let initialTodos = [
+	{
+		todo: '1st Todo list item!'
+	},
+	{
+		todo: '2nd Todo list item!'
+	},
+	{
+		todo: '3rd Todo list item!'
+	}
+];
+
+React.render(<TodoList tasks={initialTodos} />, app);
 
 console.log('running main.jsx');
